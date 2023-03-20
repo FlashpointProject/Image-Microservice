aaa4e414904f316d1cceec19b28fa0eded113c9b
Colin <colinb969@gmail.com>
2023-03-20 10:50:18 +0100

    feat: Finished precache task

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import sharp from 'sharp';
 import * as fs from 'fs';
+import prettyBytes from 'pretty-bytes';
 
 dotenv.config();
 
@@ -11,13 +12,40 @@ const port = process.env.PORT;
 const urlBase = process.env.URL_BASE || '';
 const imagesFolder = path.resolve(process.env.IMAGES_FOLDER || 'images');
 const cacheFolder = path.resolve(process.env.CACHE_FOLDER || 'cache');
+const debug = !!process.env.DEBUG;
+
+function debugPrint(text: string) {
+  if (debug) {
+    console.log(text);
+  };
+}
+
+async function dirSize(directory: string) {
+  const files = await fs.promises.readdir( directory );
+  const stats = files.map( file => fs.promises.stat( path.join( directory, file ) ) );
+  return ( await Promise.all( stats ) ).reduce( ( accumulator, { size } ) => accumulator + size, 0 );
+}
 
 async function precacheTask(folders: string[]) {
+  console.log('Started precache task for ' + folders.join('/'));
+  const startTime = new Date();
   const cachePath = path.join(cacheFolder, ...folders);
   const rootPath = path.join(imagesFolder, ...folders);
   await fs.promises.mkdir(cachePath, { recursive: true });
   await fs.promises.mkdir(cachePath, { recursive: true });
   await precacheFolder(cachePath, rootPath, rootPath);
+  const msTaken = (new Date()).getTime() - startTime.getTime();
+  console.log(`Precache finished for ${folders.join('/')}, took ${Math.floor(msTaken/1000)} seconds`);
+  if (debug) {
+    // Calculate original size
+    const orgSize = await dirSize(rootPath);
+    // Calculate cache size
+    const size = await dirSize(cachePath);
+    console.log(`Precache size for ${folders.join('/')}` +
+    `\nOriginal - ${prettyBytes(orgSize)}` +
+    `\nCached   - ${prettyBytes(size)}` +
+    `\nSaved    - ${prettyBytes(orgSize - size)} (${((size / orgSize) * 100).toFixed(2)}%)`);
+  }
 }
 
 async function precacheFolder(cachePath: string, rootPath: string, folder: string) {
@@ -75,6 +103,7 @@ async function getOrCreateCacheFile(cachePath: string, filePath: string, relPath
       }
     }
     // Cache missing, create now
+    await fs.promises.mkdir(path.dirname(destPath), { recursive: true });
     return new Promise<CacheFile>((resolve, reject) => {
       sharp(filePath)
       .toFormat(opts.type)
@@ -118,8 +147,9 @@ function genImageRoute(folders: string[]) {
           res.status(404).send('Not Found');
         }
       })
-      .catch(() => {
+      .catch((err) => {
         res.status(500).send('Error processing image');
+        console.error(err);
       });
 
     }
@@ -132,6 +162,9 @@ genImageRoute(['Screenshots']);
 app.get(`${urlBase}/`, (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+// Start precache task
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
