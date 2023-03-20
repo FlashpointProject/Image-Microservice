9d8609eb4025b14ba608b1fe8f56775b8f87d4a6
Colin <colinb969@gmail.com>
2023-03-20 10:27:34 +0100

    feat: Add precache func, cleanup

--- src/index.ts
+++ src/index.ts
@@ -12,53 +12,116 @@ const urlBase = process.env.URL_BASE || '';
 const imagesFolder = path.resolve(process.env.IMAGES_FOLDER || 'images');
 const cacheFolder = path.resolve(process.env.CACHE_FOLDER || 'cache');
 
+async function precacheTask(folders: string[]) {
+  const cachePath = path.join(cacheFolder, ...folders);
+  const rootPath = path.join(imagesFolder, ...folders);
+  await fs.promises.mkdir(cachePath, { recursive: true });
+  await fs.promises.mkdir(cachePath, { recursive: true });
+  await precacheFolder(cachePath, rootPath, rootPath);
+}
+
+async function precacheFolder(cachePath: string, rootPath: string, folder: string) {
+  const files = await fs.promises.readdir(folder, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      // Cache folder
+      const newFolder = path.join(folder, file.name);
+      await precacheFolder(cachePath, rootPath, newFolder);
+    } else {
+      // Cache file
+      const filePath = path.join(folder, file.name);
+      const relPath = path.relative(rootPath, filePath);
+      await getOrCreateCacheFile(cachePath, filePath, relPath, { type: 'jpg' }, true);
+    }
+  }
+}
+
+type CacheFile = {
+  success: boolean;
+  filePath: string;
+}
+
+async function getOrCreateCacheFile(cachePath: string, filePath: string, relPath: string, opts: ImageOpts): Promise<CacheFile> {
+  // Swap extension for one in opts for savings
+  const newFileName = path.parse(path.basename(relPath)).name + '.' + opts.type;
+  const newRelPath = path.join(path.dirname(relPath), newFileName);
+  const destPath = path.join(cachePath, newRelPath);
+
+  // If a PNG, just serve the original
+  if (opts.type === 'png') {
+    return {
+      success: true,
+      filePath
+    };
+  }
+
+  // Not a PNG, commence caching
+  return fs.promises.access(destPath, fs.constants.R_OK)
+  .then(() => {
+    return {
+      success: true,
+      filePath: destPath
+    };
+  })
+  .catch(async () => {
+    // Check source exists before caching
+    const exists = await fs.promises.access(filePath, fs.constants.R_OK)
+    .then(() => true)
+    .catch(() => false);
+    if (!exists) {
+      return {
+        success: false,
+        filePath: ''
+      }
+    }
+    // Cache missing, create now
+    return new Promise<CacheFile>((resolve, reject) => {
+      sharp(filePath)
+      .toFormat(opts.type)
+      .toBuffer((err, buffer, info) => {
+        if (err) {
+          reject(err);
+        } else {
+          fs.writeFile(destPath, buffer, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve({
+                success: true,
+                filePath: destPath
+              });
+            }
+          });
+        }
+      });
+    });
+  });
+}
+
 function genImageRoute(folders: string[]) {
   const cachePath = path.join(cacheFolder, ...folders);
+  const rootPath = path.join(imagesFolder, ...folders);
   fs.mkdirSync(cachePath, { recursive: true });
+
   app.get(`${urlBase}/${folders.join('/')}/:f1/:f2/:filename`, async (req: Request, res: Response) => {
-    const filePath = path.join(imagesFolder, ...folders, req.params.f1, req.params.f2, req.params.filename);
+    const filePath = path.join(rootPath, req.params.f1, req.params.f2, req.params.filename);
+    const relPath = path.relative(rootPath, filePath);
     if (isSubFile(imagesFolder, filePath)) {
-      const fileName = path.parse(filePath).name;
       // Check query params for options
       const opts = parseOptionQuery(req.query);
-      const newFileName = `${fileName}.${opts.type}`;
-      // Send original if PNG
-      if (opts.type === 'png') {
-        await fs.promises.access(filePath, fs.constants.R_OK)
-        .then(() => {
-          res.sendFile(filePath);
-        })
-        .catch(() => {
+
+      await getOrCreateCacheFile(cachePath, filePath, relPath, opts)
+      .then((cacheFileInfo) => {
+        if (cacheFileInfo.success) {
+          res.sendFile(cacheFileInfo.filePath);
+        } else {
           res.status(404).send('Not Found');
-        });
-        return;
-      }
-      // Check if already cached
-      const cachedFilePath = path.join(cachePath, newFileName);
-      await fs.promises.access(cachedFilePath, fs.constants.R_OK)
-      .then(() => {
-        res.sendFile(cachedFilePath);
+        }
       })
-      .catch(async () => {
-        // Make sure source exists
-        await fs.promises.access(filePath, fs.constants.R_OK)
-        .then(async () => {
-          const s = sharp(filePath);
-          return new Promise<void>((resolve) => {
-            s.toFile(cachedFilePath, (err) => {
-              if (err) {
-                res.status(500).send('Failed processing image');
-              } else {
-                res.sendFile(cachedFilePath);
-              }
-              resolve();
-            });
-          });
-        })
-        .catch(() => {
-          res.status(404).send('Not Found');
-        })
-      })
+      .catch(() => {
+        res.status(500).send('Error processing image');
+      });
+
     }
   });
 }
@@ -83,7 +146,7 @@ type ImageOpts = {
   type: typeof validTypes[number]
 }
 
-const validTypes = ['png', 'jpeg', 'jpg'] as const;
+const validTypes = ['png', 'jpg'] as const;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function parseOptionQuery(query: any) {
